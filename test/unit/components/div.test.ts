d76df45cbc4103e21c36118c94e82588e3288d3d
Nik <niknakbakpak@gmail.com>
2019-03-07 06:11:46 -0500

    add div test

---
+++ test/unit/components/div.test.ts
@@ -0,0 +1,28 @@
+import { Div } from "@src/components/div";
+
+const oldCreateElement = document.createElement;
+
+describe("Div", () => {
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    beforeAll(() => {
+        document.createElement = jest.fn().mockReturnValue({});
+    });
+
+    afterAll(() => {
+        document.createElement = oldCreateElement;
+    });
+
+    it("can be created", () => {
+        const className = "test";
+        const div = new Div(className);
+        expect(div).toBeTruthy();
+
+        expect(document.createElement).toHaveBeenCalledWith("div");
+        expect(div).toHaveProperty("element", { className });
+    });
+
+});
