2d05f033487f02e602f8ca47424f9b8d18af879d
Nik <niknakbakpak@gmail.com>
2019-03-06 21:24:08 -0500

    add test for constructor params

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,9 +1,40 @@
 import { FloatingWindow } from "../../src";
 
+const oldCreateElement = document.createElement;
+const oldQuerySelector = document.querySelector;
+
 describe("FloatingWindow", () => {
 
-    it("can be created", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    beforeAll(() => {
+        document.createElement = jest.fn();
+        document.querySelector = jest.fn();
+    });
+
+    afterAll(() => {
+        document.createElement = oldCreateElement;
+        document.querySelector = oldQuerySelector;
+    });
+
+    it("can be created without params", () => {
         const window = new FloatingWindow();
         expect(window).toBeTruthy();
+
+        expect(document.createElement).toHaveBeenCalledWith("div");
+    });
+
+    it("can be created with params", () => {
+        const selector = "#test";
+        const element = { test: 1 };
+        (document.querySelector as any).mockReturnValue(element);
+
+        const window = new FloatingWindow(selector);
+
+        expect(window).toBeTruthy();
+        expect(document.querySelector).toHaveBeenCalledWith(selector);
+        expect(window).toHaveProperty("element", element);
     });
 });
