f20c1e010751e1e2ffe20839cc2103f20e00d418
Nik <niknakbakpak@gmail.com>
2019-03-17 16:40:51 -0400

    export events

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,10 @@ export {
     IWindowIcon,
     IWindowModalOptions,
 } from "./components/window/interfaces";
+
+export { WindowModalBlurEvent } from "./events/blur";
+export { WindowModalFocusEvent } from "./events/focus";
+export { WindowModalMinimizeEvent } from "./events/minimize";
+export { WindowModalUnminimizeEvent } from "./events/unminimize";
+export { WindowModalResizeEvent } from "./events/resize";
+export { WindowModalMoveEvent } from "./events/move";
